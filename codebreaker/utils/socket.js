22a018915843def00563a5e2279096b8b33b9b20
Thomas CERDERA <thomas.cerdera@campus-igs-toulouse.fr>
2024-09-29 22:53:02 +0200

    updating socket

--- codebreaker/utils/socket.js
+++ codebreaker/utils/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Configuration du socket client
-const SOCKET_URL = "http://localhost:8000"; // Adresse de ton serveur backend
+const SOCKET_URL = "https://codebreakersback-production.up.railway.app"; // Adresse de ton serveur backend
 const socket = io(SOCKET_URL, {
   transports: ['websocket'], // Utilisation de WebSocket pour les communications
 });
