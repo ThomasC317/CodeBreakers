5864ae5e916fa56aff78526d9f5730fd13d2b639
Thomas CERDERA <thomas.cerdera@infotel.com>
2024-09-30 09:10:15 +0200

    moving files up to one folder

--- utils/socket.js
+++ utils/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Configuration du socket client
-const SOCKET_URL = "http://localhost:8000"; // Adresse de ton serveur backend
+const SOCKET_URL = "http://codebreakersback-production.up.railway.app:8000"; // Adresse de ton serveur backend
 const socket = io(SOCKET_URL, {
   transports: ['websocket'], // Utilisation de WebSocket pour les communications
 });
